ddc91150acc4e416fec0b75389ed5d147875eed2
Gracia Rizka Pasfica <graciariska1262@gmail.com>
2022-01-23 11:16:46 +0700

    Upload

---
+++ modul3/HelloWorldITTelkomPurwokerto/app/src/main/java/com/gracia/helloworldittelkompurwokerto/Halaman2Activity.kt
@@ -0,0 +1,11 @@
+package com.gracia.helloworldittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Halaman2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman2)
+    }
+}
---
+++ modul6/Modul6/app/src/main/java/com/gracia/modul6/MyData.kt
@@ -0,0 +1,7 @@
+package com.gracia.modul6
+
+data class MyData(
+    var name : String,
+    var description : String,
+    var photo : String
+)
